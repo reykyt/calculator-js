44a52e4fc12a84ad6857488997fce222011515d9
reykyt <145888308+reykyt@users.noreply.github.com>
2025-01-18 12:25:47 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,25 +4,91 @@ let input2 = document.getElementById("num2")
 
 let button = document.getElementById("convert")
 let result = document.getElementById("result")
-function one (){
+function one() {
     let number1 = Number(input1.value);
     let number2 = Number(input2.value);
-    let id= list.value;
+    let id = list.value;
     let res
-    if (id == "+"){
-        res= number1 + number2
+    if (id == "+") {
+        res = number1 + number2
     }
-    if (id == "-"){
-        res= number1 -number2
+    if (id == "-") {
+        res = number1 - number2
     }
-    if (id == "*"){
-        res= number1 * number2
+    if (id == "*") {
+        res = number1 * number2
     }
-    if (id == "/"){
-        res= number1 / number2
+    if (id == "/") {
+        res = number1 / number2
 
     }
-    result.value=res
+    result.value = res
 }
 button.addEventListener("click", one)
 console.log(list.value);
+
+input1.addEventListener("keydown", (event) => {
+    let isDigit = false;
+    let isDashedPlus = false;
+    let isControl = false;
+    if (event.key >= 0 && event.key <= 9) {
+        isDigit = true;
+
+    }
+    if (event.key == `-` || event.key == `+`) {
+        isDashedPlus = true;
+    }
+    if (event.key == `ArrowLeft` || event.key == `ArrowRight` || event.key == `Backspace` || event.key == `Delete`) {
+        isControl = true;
+    }
+    if (!isDigit && !isDashedPlus && !isControl || event.keyCode == 32) {
+        event.preventDefault();
+    }
+
+    //     console.log(event.preventDefault);
+
+})
+
+input2.addEventListener("keydown", (event) => {
+    let isDigit = false;
+    let isDashedPlus = false;
+    let isControl = false;
+    if (event.key >= 0 && event.key <= 9) {
+        isDigit = true;
+
+    }
+    if (event.key == `-` || event.key == `+`) {
+        isDashedPlus = true;
+    }
+    if (event.key == `ArrowLeft` || event.key == `ArrowRight` || event.key == `Backspace` || event.key == `Delete`) {
+        isControl = true;
+    }
+    if (!isDigit && !isDashedPlus && !isControl || event.keyCode == 32) {
+        event.preventDefault();
+    }
+
+    //     console.log(event.preventDefault);
+
+})
+
+result.addEventListener("keydown", (event) => {
+    let isDigit = false;
+    let isDashedPlus = false;
+    let isControl = false;
+    if (event.key >= 0 && event.key <= 9) {
+        isDigit = true;
+
+    }
+    if (event.key == `-` || event.key == `+`) {
+        isDashedPlus = true;
+    }
+    if (event.key == `ArrowLeft` || event.key == `ArrowRight` || event.key == `Backspace` || event.key == `Delete`) {
+        isControl = true;
+    }
+    if (!isDigit && !isDashedPlus && !isControl || event.keyCode == 32) {
+        event.preventDefault();
+    }
+
+    //     console.log(event.preventDefault);
+
+})
